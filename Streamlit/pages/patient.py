79450adcc4388b43e71ba49b0c9114554bcaff72
Sumanayana-Konda <konda.su@northeastern.edu>
2023-12-14 07:46:56 -0500

    Patient

--- Streamlit/pages/patient.py
+++ Streamlit/pages/patient.py
@@ -2,8 +2,10 @@ import streamlit as st
 import base64
 import psycopg2
 import time
-
-# Database credentials
+import requests
+import json
+import pandas as pd
+# Parameters for connecting to the database
 db_host = st.secrets["DB_HOST"]
 db_name = st.secrets["DB_NAME"]
 db_user = st.secrets["DB_USER"]
@@ -20,6 +22,8 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 gender_choices = ["Male", "Female", "Other"]
 
+fast_api_endpoint = "https://project-final-f9937888519e.herokuapp.com"
+
 # Function to set background image
 def set_bg_hack(main_bg):
     main_bg_ext = "jpeg"
@@ -35,6 +39,68 @@ def set_bg_hack(main_bg):
         unsafe_allow_html=True
     )
 set_bg_hack('/Users/sumanayanakonda/Desktop/Final-Project/Streamlit/images/blue.jpeg')
+
+# Function to get Doctor Specialies:
+
+
+def find_doctors(symptoms, age, gender, special_instructions):
+    url = 'https://project-final-f9937888519e.herokuapp.com/find-doctors/'
+    data = {
+        "symptoms": symptoms,
+        "age": age,
+        "gender": gender,
+        "special_instructions": special_instructions
+    }
+    response = requests.post(url, json=data)
+    return response.json()
+
+def initial_diagnosis(symptoms, age, gender, special_instructions):
+    url = 'https://project-final-f9937888519e.herokuapp.com/initial-diagnosis/'
+    data = {
+        "symptoms": symptoms,
+        "age": age,
+        "gender": gender,
+        "special_instructions": special_instructions
+    }
+    response = requests.post(url, json=data)
+    return response.json()
+
+def get_doctors(insurance, specialty):
+    url = 'https://project-final-f9937888519e.herokuapp.com/get_doctors/'
+    data = {
+        "insurance": insurance,
+        "specialty": specialty
+    }
+    response = requests.post(url, json=data)
+    return response.json()
+
+def display_doctors_table(doctors_data):
+
+    column_names = [
+        "DOCTORS NAME",
+        "LICENSE ID",
+        "LICENSE STATUS",
+        "ACCEPTS MEDICAID",
+        "ACCEPTS NEW PATIENTS",
+        "INSURANCE",
+        "CITY",
+        "STATE",
+        "ZIPCODE",
+        "LATITUDE",
+        "LONGITUDE",
+        "TIMEZONE",
+        "DOCTORS ID",
+        "PRACTICE NAME",
+    ]
+
+    # Create a table with the specified column names
+    filtered_data = [row[1:] for row in doctors_data]
+    st.table(pd.DataFrame(filtered_data, columns=column_names))
+
+    
+            
+            
+
 
 def check_session_timeout(session_start_time, timeout_minutes=30):
     current_time = time.time()
@@ -74,28 +140,67 @@ if st.session_state['logged_in']:
     else:
         # Display user interface
         st.title(f"Welcome, {st.session_state['username']}!")
-        st.subheader("Patient Details")
 
         # Show patient details
         patient_details = st.session_state['patient_details']
-        patient_id, first_name, last_name, age, gender, *other_details = patient_details
-        st.write(f"Patient ID: {patient_id}")
-        st.write(f"First Name: {first_name}")
-        st.write(f"Last Name: {last_name}")
-        st.write(f"Age: {age}")
-        st.write(f"Gender: {gender}")
-        # Add more patient details if needed
+        patient_id, first_name, last_name, age, has_insurance, insurance, gender, username, password = patient_details
+        with st.expander("Patient Details"):
+            st.write(f"Patient ID: {patient_id}")
+            st.write(f"First Name: {first_name}")
+            st.write(f"Last Name: {last_name}")
+            st.write(f"Age: {age}")
+            st.write(f"Gender: {gender}")
+            st.write(f"Does the Patient have Insurance?: {has_insurance}")
+            if has_insurance:
+                st.write(f"Insurance: {insurance}")
+            st.write(f"Username: {username}")
 
+        Zipcode = st.text_input("Zipcode (Optional)")
+        Symptoms = st.text_input("Symptoms")
+        AI = st.text_input("Optional: Additional Information")
         # Buttons for different information
-        st.button("Initial diagnosis", on_click=lambda: st.session_state.update({'display_content': 'initial_diagnosis'}))
-        st.button("Doctors", on_click=lambda: st.session_state.update({'display_content': 'doctors'}))
-        st.button("Hospitals", on_click=lambda: st.session_state.update({'display_content': 'hospitals'}))
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.button("Initial diagnosis", on_click=lambda: st.session_state.update({'display_content': 'initial_diagnosis'}))
+        with col2:
+            st.button("Doctors", on_click=lambda: st.session_state.update({'display_content': 'doctors'}))
+        with col3:
+            st.button("Hospitals", on_click=lambda: st.session_state.update({'display_content': 'hospitals'}))
 
         # Display content based on button click
         if st.session_state['display_content'] == 'initial_diagnosis':
-            st.write("Initial diagnosis information goes here.")
+            r = initial_diagnosis(Symptoms, age, gender, AI)
+            st.subheader("Initial Diagnosis: According to the symptoms you have entered:")
+            st.write("ranked with highest probability:")
+            with st.expander("First Hypothesis"):
+                st.write(r[0])
+            with st.expander("Second Hypothesis"):
+                st.write(r[1])
+            with st.expander("Third Hypothesis"):
+                st.write(r[2])
+        
+        
         elif st.session_state['display_content'] == 'doctors':
-            st.write("Doctor information goes here.")
+            r = find_doctors(Symptoms, age, gender, AI)
+            st.subheader("Doctors: According to the symptoms you have entered:")
+            with st.expander(r[0]):
+                re = r[0].split(" ")
+                remo = ' '.join(re[1:])
+                st.write(remo)
+                results = get_doctors(insurance, remo)
+                display_doctors_table(results)
+
+            with st.expander(r[1]):
+                remo = ' '.join(re[1:])
+                st.write(remo)
+                results = get_doctors(insurance, remo)
+                display_doctors_table(results)
+            with st.expander(r[2]):
+                remo = ' '.join(re[1:])
+                st.write(remo)  
+                results = get_doctors(insurance, remo) 
+                display_doctors_table(results)
+        
         elif st.session_state['display_content'] == 'hospitals':
             st.write("Hospital information goes here.")
 
